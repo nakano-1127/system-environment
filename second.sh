d54ba83769de0e617a02373d913bab5dad83241a
ynakano1127 <ynakano1127@gmail.com>
2018-11-17 23:23:25 +0900

    [add]OSが入って次にやるシェルスクリプトを追加

---
+++ second.sh
@@ -0,0 +1,18 @@
+# ユーザー追加
+pacman -S zsh --noconfrim
+useradd -m -G wheel -s /bin/zsh yui
+passwd yui
+visudo
+
+# GUI環境
+pacman -S xorg i3 xterm dmenu lightdm lightdm-gtk-greeter --noconfirm
+systemctl enable lightdm
+
+# dualboot
+pacman -S os-prober --noconfirm
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# 次起動したときにやっておくと便利なこと
+pacman -S git --noconfirm
+pacman -S networkmanager --noconfirm
+systemctl enable networkmanager

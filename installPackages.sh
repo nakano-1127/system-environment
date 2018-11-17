f6ddda634c1c0d937c7288d81328daa4290286d1
ynakano1127 <ynakano1127@gmail.com>
2018-11-17 18:55:36 +0900

    [add]していなかった実装をした

--- installPackages.sh
+++ installPackages.sh
@@ -9,9 +9,28 @@ pacman -S git --noconfirm
 git config --global user.email "ynakano1127@gmail.com"
 git config --global user.name "ynakano1127"
 
+#pcmanfmをインストール
+pacman -S pcmanfm
+
+#unzip
+pacman -S unzip
+
 #yaourtをインストール
+#https://archlinux.fr/yaourt-en
+cd $HOME
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+rm -rf package-query
+rm -rf yaourt
 
 #chromeをインストール
+yaourt -S google-chrome --noconfirm
 
 #puddletagをインストール
-
+yaourt -S puddletag --noconfirm

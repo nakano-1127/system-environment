923eff7daea5fd9b5c5587b93e24d46637af2b49
ynakano1127 <ynakano1127@gmail.com>
2018-11-17 18:02:31 +0900

    [add]いろんなパッケージをいれるシェルスクリプトを追加

---
+++ installPackages.sh
@@ -0,0 +1,17 @@
+#ターミナルエミュレータ"sakura"をインストール
+pacman -S sakura --noconfirm
+
+#chromiumをインストール
+pacman -S chromium --noconfirm
+
+#gitをインストール
+pacman -S git --noconfirm
+git config --global user.email "ynakano1127@gmail.com"
+git config --global user.name "ynakano1127"
+
+#yaourtをインストール
+
+#chromeをインストール
+
+#puddletagをインストール
+

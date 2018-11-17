92a6370ff71f61be4182e7f0ba4261bc82ae9c20
ynakano1127 <ynakano1127@gmail.com>
2018-11-17 21:35:23 +0900

    [fix]fcitxをなんとか動かせるようにした

--- fcitx.sh
+++ fcitx.sh
@@ -2,12 +2,12 @@ cd $HOME
 
 pacman -S fcitx-im fcitx-configtool fcitx-mozc
 
-fcitx-configtool
-
 cat > .xprofile << "EOF"
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
 EOF
 
-
+echo 'i3configにfcitxを起動するこまんどを書いてください'
+echo 'ログアウトしてログインしなおしてください'
+echo 'fcitx-configtoolを設定したらうまく動くでしょう'

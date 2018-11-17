67407270d351de2ee4f448cab232b2c11a7a593f
ynakano1127 <ynakano1127@gmail.com>
2018-11-17 23:09:50 +0900

    [add]zshの設定をするシェルスクリプトを追加

---
+++ zsh.sh
@@ -0,0 +1,15 @@
+cd $HOME
+git clone https://github.com/sanoto/zshrc
+mv zshrc .zshrc-git
+
+cat > $HOME/.zshrc << "EOF"
+FPATH="${HOME}/.zshrc-git:${FPATH}"
+
+autoload -Uz zshrc-base && zshrc-base
+autoload -Uz zshrc-zplug && zshrc-zplug
+autoload -Uz zshrc-prompt && zshrc-prompt
+autoload -Uz zshrc-alias && zshrc-alias
+autoload -Uz zshrc-python && zshrc-python
+EOF
+
+sakura

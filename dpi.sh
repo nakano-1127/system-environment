02a89139b806c9458f6ad5d4f8fe8ba7b1ea30c2
ynakano1127 <ynakano1127@gmail.com>
2018-11-17 21:05:13 +0900

    [update]dpi.sh

--- dpi.sh
+++ dpi.sh
@@ -1,8 +1,16 @@
-pacman -S xf86-video-intel
+cd `dirname $0`
 
-mv ./xorg.conf.d/* /etc/X11/xorg.conf.d/
+pacman -S xf86-video-intel --noconfirm
 
-cat 'export GDK_DPI_SCALE=1.5' >> $HOME/.xprofile
-cat 'export QT_AUTO_SCREEN_SCALE_FACTOR=1' >> $HOME/.xprofile
+cp xorg.conf.d/* /etc/X11/xorg.conf.d/
+
+if [ ! -f $HOME/.xprofile ]; then
+    # 存在しない場合
+    touch $HOME/.xprofile
+fi
+
+
+echo 'export GDK_DPI_SCALE=1.5' >> $HOME/.xprofile
+echo 'export QT_AUTO_SCREEN_SCALE_FACTOR=1' >> $HOME/.xprofile
 
 echo 'rebootしてください'

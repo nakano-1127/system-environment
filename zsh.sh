5b49efdc2ff401edb722b0e648ba0327b5df0b8e
ynakano1127 <ynakano1127@gmail.com>
2018-11-18 18:52:53 +0900

    [rm]/dev/null

--- zsh.sh
+++ zsh.sh
@@ -5,11 +5,11 @@ mv zshrc .zshrc-git
 cat > $HOME/.zshrc << "EOF"
 FPATH="${HOME}/.zshrc-git:${FPATH}"
 
-autoload -Uz zshrc-base && zshrc-base > /dev/null
-autoload -Uz zshrc-zplug && zshrc-zplug > /dev/null
-#autoload -Uz zshrc-prompt && zshrc-prompt > /dev/null
-autoload -Uz zshrc-alias && zshrc-alias > /dev/null
-autoload -Uz zshrc-python && zshrc-python > /dev/null
+autoload -Uz zshrc-base && zshrc-base
+autoload -Uz zshrc-zplug && zshrc-zplug
+#autoload -Uz zshrc-prompt && zshrc-prompt
+autoload -Uz zshrc-alias && zshrc-alias
+autoload -Uz zshrc-python && zshrc-python
 EOF
 
 sakura
